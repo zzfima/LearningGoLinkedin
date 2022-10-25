56025379749786608efae2506f329ca2a0a850bd
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-10-25 16:20:02 +0300

    start do slices

--- part_3/arrays.go
+++ part_3/arrays.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func main_7() {
 	var colors [3]string
 	colors[0] = "red0"
 	colors[1] = "red1"
@@ -10,4 +10,6 @@ func main() {
 	fmt.Println(colors[1])
 	fmt.Println(colors)
 
+	numbers := [4]int{4, 44, 444, 4444}
+	fmt.Println(numbers, len(numbers))
 }
