177d9c8132a7b430e47458def56b8a687adb31ad
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-10-26 09:06:12 +0300

    add slices

--- part_3/slices.go
+++ part_3/slices.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+)
 
+func main_8() {
+	//usual array
+	colors := [3]string{"red", "green", "blue"}
+	fmt.Println(colors)
+	fmt.Printf("%T\n", colors)
+
+	//slice
+	numbers := []int{4444, 44, 444}
+	fmt.Println(numbers)
+	fmt.Printf("%T\n", numbers)
+	//append numbers
+	numbers = append(numbers, 5)
+	fmt.Println(numbers)
+
+	//sorting
+	sort.Ints(numbers)
+	fmt.Println(numbers)
 }
