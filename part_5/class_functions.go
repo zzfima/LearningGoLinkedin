b9e04504e4833972fb8d0beac4dc81092b605254
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-10-27 15:07:09 +0300

    finish functions

--- part_5/class_functions.go
+++ part_5/class_functions.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
-func main() {
+func main16() {
 	dog := Dog{"Mike", 5.6}
 	dog.PrintInfo()
+
+	dog.SpeakThreeTimes()
+	dog.SpeakThreeTimes()
+
+	dog.SpeakFourTimes()
+	dog.SpeakFourTimes()
 }
 
+//Dog describe dog
 type Dog struct {
 	Name string
 	Age  float32
 }
 
+//PrintInfo print info about dog
 func (d Dog) PrintInfo() {
 	fmt.Println("My name is:", d.Name, ", my age is:", d.Age)
+}
+
+//SpeakThreeTimes 3 times print name. d object passed  by value
+func (d Dog) SpeakThreeTimes() {
+	d.Name = fmt.Sprintf("%v %v %v", d.Name, d.Name, d.Name)
+	fmt.Println(d.Name)
+}
+
+//SpeakFourTimes 4 times print name. d object passed  by reference
+func (d *Dog) SpeakFourTimes() {
+	d.Name = fmt.Sprintf("%v %v %v %v", d.Name, d.Name, d.Name, d.Name)
+	fmt.Println(d.Name)
 }
--- part_5/functions.go
+++ part_5/functions.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-func main_15() {
-	res := add_two_numbers(3, 4)
+func main15() {
+	res := addTwoNumbers(3, 4)
 	fmt.Println(res)
 
-	res, l := add_numbers(4, 5, 6, 7)
+	res, l := addNumbers(4, 5, 6, 7)
 	fmt.Println(res, l)
 }
 
-func add_two_numbers(v1 int, v2 int) int {
+func addTwoNumbers(v1 int, v2 int) int {
 	return v1 + v2
 }
 
-func add_numbers(values ...int) (int, int) {
+func addNumbers(values ...int) (int, int) {
 	sum := 0
 
 	for i := range values {
